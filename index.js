2d9711ce692b78287025ae8ff9c9fa581993d5da
Jashan Sudan <jashansudan@gmail.com>
2017-01-16 17:28:19 -0800

    trying to bring back alive

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const request = require('request')
 var Yelp = require('yelp');
 const app = express()
 
-const yelpTokenSecret = process.env.YELP_TOKEN_SECRET
-const yelpConsumerSecret = process.env.YELP_CONSUMER_SECRET
-const facebookToken = process.env.FB_PAGE_ACCESS_TOKEN
+const yelpConsumerSecret = process.env.YELP_CONSUMER_SECRET;
+const yelpTokenSecret = process.env.YELP_TOKEN_SECRET;
+const facebookToken = process.env.FB_PAGE_ACCESS_TOKEN;
 
 //Yelp
 var yelp = new Yelp({
@@ -79,7 +79,7 @@ function sendTextMessage(sender, text) {
 }
 
 function testSearch(){
-
+	console.log(test)
 }
 
 
