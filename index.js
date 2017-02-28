09c0790565af616291d2ab46c95a1eeb959b17bf
Jashan Sudan <jashansudan@gmail.com>
2017-02-28 00:01:14 -0800

    Removed unused function

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 var Yelp = require('yelp');
-const app = express()
+const app = express();
 
 const yelpTokenSecret = process.env.YELP_TOKEN_SECRET
 const yelpConsumerSecret = process.env.YELP_CONSUMER_SECRET
 const facebookToken = process.env.FB_PAGE_ACCESS_TOKEN
 
-//Yelp
+//Yelp node package code
 var yelp = new Yelp({
   consumer_key: 'Qq97hE7r8TaIMbStTIpGpQ',
   consumer_secret: yelpConsumerSecret,
@@ -34,35 +34,33 @@ app.get('/', function (req, res) {
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token')
+    res.send('Error, wrong token');
 })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+    console.log('running on port', app.get('port'));
 })
 
 //To process messages
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
+        let event = req.body.entry[0].messaging[i];
+        let sender = event.sender.id;
         if (event.message && event.message.text) {
-            let text = event.message.text
-            queryYelp(sender, text)
+            let text = event.message.text;
+            queryYelp(sender, text);
             //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
         }
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
 })
-
 
 //Query yelp with your message
 function queryYelp(sender, message) {
-    // {term: 'yelp', location: 'sf', limit: 1}
     let query = searchWithOnlyLocation(message);
     yelp.search(query).then(function (data) {
         console.log(data);
@@ -73,36 +71,21 @@ function queryYelp(sender, message) {
     });
 }
 
+// Sets predfined queries and simply takes the users location to produce outputs
+function searchWithOnlyLocation(message){
+    let searchQuery = {term: "food", limit: 5};
+    searchQuery["location"] = message;
+    return searchQuery;
+}
 
 //Itterates through all businesses returned and checks to see if they are worthy (3.6 stars or higher)
 function checkBussinessRatingBeforeSending(data, sender){
     for (let i = 0; i < data.businesses.length; i++){
             if (data.businesses[i].rating > 3.6){
                 sendMessengerCard(sender,  data.businesses[i]);
-            }
         }
+    }
 }
-
-// Sends a message to the user
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:facebookToken},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
 
 // Sends a card message back to the user
 function sendMessengerCard(sender, data) {
@@ -124,7 +107,7 @@ function sendMessengerCard(sender, data) {
     })
 }
 
-//Missing the [i] lol
+//Tekes the query results, and converts it into a sendable card
 function convertToSendable(data) {
     let messageData = {
         attachment:{
@@ -149,12 +132,27 @@ function convertToSendable(data) {
     return messageData;
 }
 
-// Sets predfined queries and simply takes the users location to produce outputs
-function searchWithOnlyLocation(message){
-    let searchQuery = {term: "food", limit: 5};
-    searchQuery["location"] = message;
-    return searchQuery;
+// Sends a message to the user, requires more specific input in the text field
+// Currently not being used as it's been swapped out for Cards
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:facebookToken},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    })
 }
+
 
 
 //Takes the user input and parses it into a JSON object that can be used to query
@@ -176,10 +174,11 @@ function parseInput(message){
 
 
 
-// TODO: Check if the user's query is valid
-function validQueryCheck(){
-
-}
+//TODO: Have multiple input analysis, so that when a user starts a conversation
+// a prompt is sent, and then followed up by asking for location and finally,
+// a response is returned with nearby food places
+// TODO: Database integration
+// Things to think about: Machine learning prediction model
 
 
 //test query: {term: 'yelp', location: 'sf', limit: 1}
