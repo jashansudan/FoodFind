574162bac77719a023b8800a8859f4809506d94e
Jashan Sudan <jashansudan@gmail.com>
2017-04-28 09:14:24 -0700

    handling lattitude

--- index.js
+++ index.js
@@ -50,10 +50,11 @@ app.post('/webhook/', function (req, res) {
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i];
         let sender = event.sender.id;
-        if (event.message && event.message.text) {
-            sendTextMessage(sender, "Your lat is ");
+        if (event.message && event.message.attachments[0].payload) {
+          let lat = event.message.attachments[0].payload.coordinates.lat;
+          sendTextMessage(sender, "Your lat is " + lat);
         }
-        else if (event.message && event.message.attachments[0].payload){
+        else if (event.message && event.message.text){
           requestLocation(sender)
         }
     }
